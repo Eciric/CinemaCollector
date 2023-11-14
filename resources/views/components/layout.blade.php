0b9b38983d4532bd8fe53765ccafb018fcb82c6d
Przemysław Kaczmarski <nyviel111@gmail.com>
2023-11-14 22:09:20 +0100

    Changed logo, added register to the navigation

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CinemaCollector</title>
-
+    <link rel="icon" type="image/png" href="{{asset('images/logo.png')}}" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
         integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="{{asset('css/home.css')}}">
 </head>
 
-<body class="bg-slate-900 text-white">
+<body class="bg-indigo-900 text-white">
     <x-navigation />
     {{$slot}}
 
--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,18 +1,22 @@
 <header id="primary-header"
-    class="primary-header minimize | relative bg-slate-950 min-h-full p-4 flex justify-start items-center flex-col shadow-lg">
+    class="primary-header minimize | relative bg-indigo-950 min-h-full p-4 flex justify-start items-center flex-col shadow-lg">
     <div onclick="minimize()" class="absolute top-2 right-4 cursor-pointer"><i id="minimize-icon"
             class="fa-solid fa-angles-left text-white"></i>
     </div>
-    <a href="#"><img class="w-16 rounded-full" src="{{asset('images/logo.png')}}" alt="Logo" /></a>
-    <h1 class="py-2">CinemaCollector</h1>
+    <a href="#"><img class="w-14" src="{{asset('images/logo.png')}}" alt="Logo" /></a>
+    <h1 class="py-2 font-bold text-lg">CinemaCollector</h1>
+    <hr class="h-px w-1/2 my-4 bg-gray-200 border-0">
     <nav #primaryNavigation class="primary-navigation grow flex flex-col">
-        <ul id="primary-navigation" role="list" class="grow py-2 flex flex-col justify-between"
+        <ul id="primary-navigation" role="list" class="grow py-2 flex flex-col justify-between items-center"
             aria-label="primary navigation">
             <div>
                 <li class="py-1 cursor-pointer">Home</li>
                 <li class="cursor-pointer">About</li>
             </div>
-            <li class="cursor-pointer">Login</li>
+            <div class="flex space-x-4">
+                <li class="cursor-pointer">Login</li>
+                <li class="cursor-pointer">Register</li>
+            </div>
         </ul>
     </nav>
 </header>
